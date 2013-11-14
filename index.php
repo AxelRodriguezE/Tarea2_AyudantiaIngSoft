cd4b868332496f819100b43e9898f9ba8cb22c69
AxelRodriguezE <axel.rodriguez@ceinf.cl>
2013-11-13 23:35:09 -0300

    Subiendo archivos php y css de prueba

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Grupo 06 Ingenieria de Software 2do S. 2013</title>
+        <base href="http://www.w3schools.com/images/" target="_blank">
+        <meta name="keywords" content="HTML, CSS, PHP, UTEM, Ingenieria de software, Ayudantia">
+        <meta name="description" content="Web de integrantes de grupo 6 de Ingenieria de software 2do semestre UTEM 2013">
+        <meta http-equiv="refresh" content="30">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+<body>
+    <div id="header">
+        Titulo
+    </div>
+    <div>
+        <p class="center">Axel</p>
+    </div>
+    <div>
+        Nico
+    </div>
+    <div>
+        Pedro
+    </div>
+    <div>
+        Frase
+    </div>
+<?php
+echo 'hola';
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+</body>
+
+</html>
